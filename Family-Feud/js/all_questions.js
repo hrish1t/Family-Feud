6dc1c8b7fb247f1e87380147578450fd778381e8
kp348 <kushagrapapnai@gmail.com>
2026-09-14 16:13:57 +0530

    Add files via upload

---
+++ Family-Feud/js/all_questions.js
@@ -0,0 +1,55 @@
+var json = [
+    {
+        "question": "Academic blocks with the most chilled water",
+        "answers": ["GDN", "MGR", "SMV", "TT"],
+        "points": [40, 30, 20, 10]
+    },
+    {
+        "question": "Hardest academic block to find your class in",
+        "answers": ["MGB", "PRP", "GDN", "SJT"],
+        "points": [40, 30, 20, 10]
+    },
+    {
+        "question": "Name something that can ruin a VITian's entire day in 5 seconds",
+        "answers": ["CAT marks", "Quiz tomorrow", "Sunday's Dinner"," Warden's call"],
+        "points": [40, 30, 20, 10]
+    },
+    {
+        "question": "Something that happens at VIT that sounds fake to outsiders",
+        "answers": ["Paid Mess", "Exploiting 9 point", "9 PM curfew", "Outing ban"],
+        "points": [40, 30, 20, 10]
+    },
+    { 
+        "question": "Name a food item that mysteriously tastes different depending on which mess you eat at.",
+        "answers": ["Chicken Biryani", "Chapati","Poriyal","Dosa"], 
+        "points": [40, 30, 20, 10] },
+    { 
+        "question": "Name something found in a VITian's room at 2 AM.",
+         "answers": ["shouting roommate", "Kettle", "Energy drink", "Warden"],
+        "points": [40, 30, 20, 10] },
+
+    { "question": "Best night mess item", 
+      "answers": ["bread omellete", "coffee", "peri peri fries", "dosa"],
+      "points": [40, 30, 20, 10] },
+
+    { "question": "Name something you see in a group project/hackathon", 
+      "answers": ["AI slopt PPT", "Absent team member", "Future prospects", "Uninterested professor"], 
+      "points": [40, 30, 20, 10] },
+
+    { "question": "Name something you would rather do than attend a 8 am class.", 
+      "answers": ["sleep", "eat food", "roam  campus", "netflix"],
+       "points": [40, 30, 20, 10] },
+       
+    { "question": "Top excuses for missing assignments ",
+      "answers": ["VTOP crashed", "Wrong PDF uploaded", "At a hackathon", "sorry prof pls check my mail "], 
+      "points": [40, 30, 20, 10] },
+    { "question": "name something VITians pretend to understand", 
+      "answers": ["lecture", "question paper", "code of conduct", "directions to class"], 
+      "points": [40, 30, 20, 10] },
+    { "question": "Name something a final-year student says that terrifies juniors", 
+      "answers": ["Placements", "arrear", "fines", "CGPA"],
+      "points": [40, 30, 20, 10] },
+    { "question": "Name a place on campus where people go to study but absolutely do not study",
+      "answers": ["Library", "Class", "Hostel room", "Labs"],
+      "points": [40, 30, 20, 10] },
+];
